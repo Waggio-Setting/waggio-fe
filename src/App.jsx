c12f002fb7805207b6d0ea9131e411726522eab5
Jiwonhmm <152750068+Jiwonhmm@users.noreply.github.com>
2025-07-17 23:53:59 +0000

    creat User Input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import UserInputForm from './component/UserInputForm'
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return <>아싸!</>;
+
+  return (
+
+  <>
+  
+  <UserInputForm />
+  
+  </>);
 }
 
 export default App;
