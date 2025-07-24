02707bc2ccd9a90b4279c7e28b44069a9992e747
Hyoan Moon <tjdud24as@gmail.com>
2025-07-24 04:00:04 -0400

    Add CSV upload and parse functionality with state storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import UserInputForm from "./component/UserInputForm";
+import CSVUpload from "./component/CSVUpload";
 
 function App() {
   return (
     <div>
-      <UserInputForm />
+      <CSVUpload />
     </div>
   );
 }
